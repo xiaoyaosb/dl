f29060f3f0d7a7ce4b34d5392fa8d6e2c4baefa4
xiaoyaosb <1353537901@qq.com>
2020-02-25 20:17:47 +0800

    Add files via upload

---
+++ cnn1.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn as nn
+
+
+def corr2d(X, K): # K 是卷积核
+    H, W = X.shape
+    h, w = K.shape
+    Y = torch.zeros(H+1-h, W+1-w)
+    for i in range(Y.shape[0]):
+        for j in range(Y.shape[1]):
+            Y[i, j] = (X[i: i + h, j: j + w] * K).sum()
+    return Y
+
+'''
+X = torch.tensor([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
+K = torch.tensor([[0, 1], [2, 3]])
+Y = corr2d(X, K)
+print(Y)
+'''
+#二维卷积层将输入和卷积核做互相关运算，并加上一个标量偏置来得到输出。卷积层的模型参数包括卷积核和标量偏置。
+
+
+class Conv2D(nn.Module):
+    def __init__(self, kernel_size):
+        super(Conv2D, self).__init__()
+        self.weight = nn.Parameter(torch.randn(kernel_size))
+        self.bias = nn.Parameter(torch.randn(1))
+
+    def forward(self, x):
+        return corr2d(x, self.weight) + self.bias
+
+
+X = torch.ones(6, 8)
+Y = torch.zeros(6, 7)
+X[:, 2: 6] = 0
+Y[:, 1] = 1
+Y[:, 5] = -1
+print(X)
+print(Y)
+
+conv2d = Conv2D(kernel_size=(1, 2))
+step = 30
+lr = 0.01
+for i in range(step):
+    Y_hat = conv2d(X)
+    l = ((Y_hat - Y) ** 2).sum()
+    l.backward()
+    # 梯度下降
+    conv2d.weight.data -= lr * conv2d.weight.grad
+    conv2d.bias.data -= lr * conv2d.bias.grad
+
+    # 梯度清零
+    conv2d.weight.grad.zero_()
+    conv2d.bias.grad.zero_()
+    if (i + 1) % 5 == 0:
+        print('Step %d, loss %.3f' % (i + 1, l.item()))
+
+print(conv2d.weight.data)
+print(conv2d.bias.data)
